a79f99461ba1a5cdd632b771d9823f8e8652e7de
Ushma Vaidya <uvaidya96@gmail.com>
2026-06-17 09:13:05 -0400

    fixed path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/uvaidya1/ushSocialMedia',
+  base: '/ushSocialMedia',
   build: {
     rollupOptions: {
       output: {
